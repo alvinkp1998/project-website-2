f5cbc7645170e41f9d13728166fb81cc4803012b
AlvinKP <alvinkusuma98@gmail.com>
2021-10-02 23:53:17 +0700

    update home

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,17 @@
 <template>
-  <div></div>
+  <div>
+    <div class="box">
+      <h3 class="center">Welcome to Fullstack Academy</h3>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.box {
+  display: flex;
+  height: 30vh;
+}
+.center {
+  margin: auto;
+}
+</style>
