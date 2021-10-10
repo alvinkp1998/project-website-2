d201722070ce9ae7ef3bb9cced7dec23d7a72b20
1672028 ALVIN KUSUMA PUTRA <it1672028@student.it.maranatha.edu>
2021-10-10 21:48:04 +0700

    tambah detail kelas-anggota

---
+++ components/anggota.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="card rounded">
+      <img
+        :src="img"
+        class="card-img-top"
+        width="100%"
+        height="200px"
+        :alt="role"
+      />
+      <div class="row px-2">
+        <div class="col">
+          <h5 class="p-0 pt-2 m-0">{{ nama }}</h5>
+          <p class="">{{ role }}</p>
+        </div>
+        <div class="col-2 d-flex align-items-center">
+          <i
+            class="fas fa-ellipsis-v pointer"
+            data-toggle="dropdown"
+            :style="`font-size:1em`"
+          ></i>
+          <div class="dropdown-menu">
+            <a class="dropdown-item">Sunting</a>
+            <a class="dropdown-item">Hapus</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    nama: String,
+    role: String,
+    img: String
+  }
+};
+</script>
---
+++ pages/_classId/anggota.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="container">
+    <h4>Tutor & SPV</h4>
+    <hr />
+    <div class="row">
+      <div
+        class="col-sm-4 col-md-3 col-6"
+        v-for="(tutorspv, i) in tutorspvs"
+        :key="i"
+      >
+        <anggota
+          :nama="tutorspv.nama"
+          :role="tutorspv.role"
+          :img="tutorspv.img"
+        />
+      </div>
+    </div>
+
+    <h4>Student</h4>
+    <hr />
+    <div class="row">
+      <div
+        class="col-sm-4 col-md-3 col-6"
+        v-for="(student, i) in students"
+        :key="i"
+      >
+        <anggota :nama="student.nama" :role="student.role" :img="student.img" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tutorspvs: [
+        {
+          nama: "Ghany Ersa Abdillah",
+          role: "Tutor",
+          img:
+            "https://cdnt.orami.co.id/unsafe/468x288/cdn-cas.orami.co.id/parenting/images/gong_yoo_korea.width-800.jpg"
+        },
+        {
+          nama: "Atika Rahmawati",
+          role: "SPV",
+          img:
+            "https://asset.kompas.com/crops/qEzp1lRjF_h3AOjVv8rhNwzoomU=/0x500:1000x1167/750x500/data/photo/2018/08/19/2853283469.jpg"
+        }
+      ],
+      students: [
+        {
+          nama: "Alvin Kusuma Putra",
+          role: "Student",
+          img:
+            "https://cdn06.pramborsfm.com/storage/app/media/Prambors/Editorial/JI%20CHANG%20WOOK-20210726122921.jpg?tr=w-800"
+        },
+        {
+          nama: "Alvin Kusuma Putra",
+          role: "Student",
+          img:
+            "https://cdn06.pramborsfm.com/storage/app/media/Prambors/Editorial/JI%20CHANG%20WOOK-20210726122921.jpg?tr=w-800"
+        },
+        {
+          nama: "Alvin Kusuma Putra",
+          role: "Student",
+          img:
+            "https://cdn06.pramborsfm.com/storage/app/media/Prambors/Editorial/JI%20CHANG%20WOOK-20210726122921.jpg?tr=w-800"
+        },
+        {
+          nama: "Alvin Kusuma Putra",
+          role: "Student",
+          img:
+            "https://cdn06.pramborsfm.com/storage/app/media/Prambors/Editorial/JI%20CHANG%20WOOK-20210726122921.jpg?tr=w-800"
+        },
+        {
+          nama: "Alvin Kusuma Putra",
+          role: "Student",
+          img:
+            "https://cdn06.pramborsfm.com/storage/app/media/Prambors/Editorial/JI%20CHANG%20WOOK-20210726122921.jpg?tr=w-800"
+        },
+        {
+          nama: "Alvin Kusuma Putra",
+          role: "Student",
+          img:
+            "https://cdn06.pramborsfm.com/storage/app/media/Prambors/Editorial/JI%20CHANG%20WOOK-20210726122921.jpg?tr=w-800"
+        },
+        {
+          nama: "Alvin Kusuma Putra",
+          role: "Student",
+          img:
+            "https://cdn06.pramborsfm.com/storage/app/media/Prambors/Editorial/JI%20CHANG%20WOOK-20210726122921.jpg?tr=w-800"
+        },
+        {
+          nama: "Alvin Kusuma Putra",
+          role: "Student",
+          img:
+            "https://cdn06.pramborsfm.com/storage/app/media/Prambors/Editorial/JI%20CHANG%20WOOK-20210726122921.jpg?tr=w-800"
+        }
+      ]
+    };
+  }
+};
+</script>
